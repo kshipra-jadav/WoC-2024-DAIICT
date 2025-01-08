2ecd507862ef353402d595baf9c24891c404f2f5
kshipra-jadav <jadavkshipra2@gmail.com>
2025-01-08 08:58:57 +0530

    checkpoint-1: almost completed the task. Vectorization and integration of utils.py in DataLoader.py is left.

--- checkpoint-1/DataLoader.py
+++ checkpoint-1/DataLoader.py
@@ -11,15 +11,21 @@ class DataLoader:
 
         else:
             self.df: pd.DataFrame = pd.read_csv(self.FILE_NAME)
+            self.df['category'] = self.df['category'].map(lambda x: x.replace('_', ' ')[: -2])
 
+    def remove_duplicates(self) -> None:
+        if self.df.shape[0] > self.df.drop_duplicates().shape[0]:
+            print('Dropping Duplicates!')
+            self.df = self.df.drop_duplicates()
 
     def preview_data(self, nrows: int = 5) -> None:
-        print(self.df.head(nrows))
+        print(self.df.sample(nrows))
 
 
 
 # for debugging purpose only
 if __name__ == '__main__':
     dl = DataLoader()
+    dl.remove_duplicates()
 
-    dl.preview_data()
+    dl.preview_data(10)
--- checkpoint-1/utils.py
+++ checkpoint-1/utils.py
@@ -1,2 +1,51 @@
+import re
+import time
+
 import pandas as pd
-import
+from nltk.tokenize import ToktokTokenizer
+from nltk.corpus import stopwords as stp
+from nltk.stem import WordNetLemmatizer
+
+def text_normalize(samples: pd.Series) -> pd.Series:
+    samples = samples.apply(lambda x: x.lower()) # convert all samples to lower case
+
+    samples = samples.apply(lambda x: re.sub(r'[^\w\s]', '', x)) # replace any character which is not string or space with blank
+
+    return samples
+
+def text_tokenize(samples: pd.Series) -> pd.Series:
+    toktok = ToktokTokenizer() # Much better speed than word_tokenize
+    samples = samples.apply(lambda x: toktok.tokenize(x))
+
+    return samples
+
+def remove_stopwords(samples: pd.Series) -> pd.Series:
+    stopwords = set(stp.words('english'))
+
+    samples = samples.apply(lambda x: [word for word in x if word not in stopwords])
+
+    return samples
+
+def text_lemmatize(samples: pd.Series) -> pd.Series:
+    wnl = WordNetLemmatizer()
+
+    samples = samples.apply(lambda x: [wnl.lemmatize(word) for word in x])
+
+    return samples
+
+
+def main():
+    df = pd.read_csv('fakeReviewData.csv')
+
+    df['text_'] = text_normalize(df['text_'])
+    df['text_'] = text_tokenize(df['text_'])
+    df['text_'] = remove_stopwords(df['text_'])
+
+    start = time.perf_counter()
+    df['text_'] = text_lemmatize(df['text_'])
+    print(f"Took - {time.perf_counter() - start:.3f} secs")
+
+
+
+if __name__ == '__main__':
+    main()
